b8d42baccdb82ad1569da26289bcbf09c5286f36
wusimon51 <wusimon51@gmail.com>
2020-12-28 17:40:44 -0500

    added HW2

---
+++ HW2.py
@@ -0,0 +1,32 @@
+# YOUR NAME
+
+'''
+Problem 1
+Implement the rest of calculateTax to compute the price of an item after tax is applied to it. New York's tax rate
+is 8.875%.
+'''
+
+def calculateTax(price):
+# Your code here
+
+
+'''
+Problem 2
+Write a function named computeHypotenuse that takes in two ints a, b in a right triangle and computes the length of the
+hypotenuse.
+'''
+
+
+'''
+Problem 3
+Write a function that takes the Euclidean distance between two points. Its parameters should be x1, y1, x2, and y2. 
+You should look up the distance formula if you forgot or don't know what it is. 
+'''
+
+
+'''
+Problem 4
+Write a function that determines whether a year is a leap year. There are two rules: the year has to be divisible by
+4, unless it is a multiple of 100, where only years divisible by 400 are leap years. So, 2000 is a leap year, but 
+1900 is not.
+'''
