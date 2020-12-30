8404d972c2387973ce806e948c02dd072793e45d
wusimon51 <wusimon51@gmail.com>
2020-12-29 22:01:08 -0500

    changed directory structure for testing

---
+++ Homework/Tests/test_HW2.py
@@ -0,0 +1,5 @@
+from .. import HW2
+
+
+def test_calculate_tax():
+    assert HW2.calculate_tax(5.50) == None
--- Tests/test_HW2.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from ..HW2 import *
-
-def test_calculate_tax():
-    assert calculate_tax()
