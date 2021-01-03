de8000f4ff79644faef66d4ac6c4bb39dd37df44
wusimon51 <wusimon51@gmail.com>
2021-01-02 20:34:23 -0500

    added HW3

---
+++ Homework/HW3.py
@@ -0,0 +1,43 @@
+# YOUR NAME HERE
+
+"""
+Problem 1
+Return the range of a list of ints. Assume that it is sorted.
+"""
+
+
+def list_range(arr):
+    return None
+
+
+"""
+Problem 2
+Switch the first two elements of a list and return the new list. Assume that every list will have at least two 
+elements.
+"""
+
+
+def switch(arr):
+    return None
+
+
+"""
+Problem 3
+Return the Chinese zodiac as a string corresponding to the year given as an int.
+"""
+
+
+def zodiac(year):
+    return None
+
+
+"""
+Problem 4
+In one line of code, return every 7th number starting from the last element going backwards. You should only need 
+to modify the return statement to do this. The result should be 777, 770, ... all the way to 0. 
+"""
+
+
+def seventh_number():
+    arr = list(range(0, 778))
+    return None
