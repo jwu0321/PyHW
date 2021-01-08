e098edf09b137a3cc9d6bedc13c2f74f6b1614c7
wusimon51 <wusimon51@gmail.com>
2021-01-07 21:28:40 -0500

    added Lesson 4

---
+++ Lesson4.py
@@ -0,0 +1,51 @@
+# br0ther may i have some l00ps
+
+i = 0
+# while i < 10:
+#     print('br0ther may i have some l00ps')
+#     i += 1  # i = i + 1
+
+# while True:
+#     print('four is greater than four')
+
+
+# l = [1, 5, 7, 9, 12]
+# i = 0
+# while i < len(l):
+#     print(l[i])
+#     i += 1
+#
+# print('backwards')
+# j = len(l) - 1
+# while j >= 0:
+#     print(l[j])
+#     j -= 1
+
+
+arr = [-5, 2, 29, -69, 100, 5, -4]
+i = 0
+count = 0
+while i < len(arr):
+    if arr[i] % 2 == 0:
+        count += 1
+    i += 1
+
+# print(count)
+
+
+i = 1
+while i <= 50:
+    # print(i ** 2)
+    i += 1
+
+
+# arr = ['one', 'two', 'three', 'four']
+# for num in arr:  # iterating
+#     print(num)
+#
+#
+# for x in range(1, 11):
+#     print(x * 10)
+
+for char in 'steve':
+    print(char)
