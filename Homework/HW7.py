7bd2004308bc7c38590478dcbfbb45381db53d22
wusimon51 <wusimon51@gmail.com>
2021-01-22 17:48:11 -0500

    added tests for HW7

--- Homework/HW7.py
+++ Homework/HW7.py
@@ -16,9 +16,9 @@ Key   Character
 9     WXYZ
 0     Space
 For instance, pressing 4 twice gets 'H'.
-Implement the function to use a dictionary to turn a string into button presses. 'Hello, World!' becomes
+Implement the function to use a dictionary to turn a string into a string of button presses. 'Hello, World!' becomes
 4433555555666110966677755531111. Account for both uppercase and lowercase letters. The button's don't differentiate
-for capitalization, you can adjust the input for the table.
+for capitalization, you can adjust the input for the table. You can ignore every other that's not shown in the table.
 """
 
 
@@ -39,7 +39,8 @@ def unique_characters(string):
 
 """
 Problem 3
-Create a Car class with the attributes max_speed, current_speed, and brand. Add two methods, accelerate and decelerate,
-and add 5 to current_speed in accelerate and subtract 5 in decelerate. Check if accelerating will go past max_speed
+Create a Car class with the attributes brand, max_speed, and current_speed. Initialize all those attributes in its
+constructor in that order. Add two methods, accelerate and decelerate, which take no parameters, 
+and add 10 to current_speed in accelerate and subtract 5 in decelerate. Check if accelerating will go past max_speed 
 and if decelerating will make it go past 0. If either situation happens, don't change the speed.
 """
